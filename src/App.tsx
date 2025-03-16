9d712e5ccd6995030948240ca117df39b5433353
Prthmsh7 <Prathmeshshukla2106@gmail.com>
2025-03-16 11:45:49 +0530

    removed few unused things

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import { useState } from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, PaletteMode } from '@mui/material';
 import { getTheme } from './theme';
