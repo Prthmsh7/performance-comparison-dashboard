628a82f38cc13f0d90a8df09ece25143224eb079
Manas Singh <manas@MacBook-Air.local>
2025-03-17 18:02:12 +0530

    fixed build errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'; 
+import { useMemo, useState } from 'react'; 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider, CssBaseline, PaletteMode } from '@mui/material';
 import { getTheme } from './theme';
